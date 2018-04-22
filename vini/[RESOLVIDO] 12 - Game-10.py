7e2ee881e3684c4443be5518bba73b3ba9ccb0e2
Vinicius Cosendey <vicosendey@gmail.com>
2018-04-22 14:18:38 -0300

    [RESOLVIDO] 12 - Game-10

---
+++ vini/[RESOLVIDO] 12 - Game-10.py
@@ -0,0 +1,17 @@
+nPos = int(input(""))
+dPos = int(input(""))
+aPos = int(input(""))
+
+pressCont = 0
+
+if dPos > aPos:
+    for i in range(aPos, dPos):
+        pressCont += 1
+elif dPos < aPos:
+    for i in range(aPos, nPos):
+        pressCont += 1
+    for i in range(0, dPos):
+        pressCont += 1
+
+
+print(pressCont)
